e77245d011bda867e7152217cd05a253fd998f41
Marco Ris <ris@contria.com>
2020-08-04 08:01:23 +0200

    Update script with installing missing commands

--- recon.sh
+++ recon.sh
@@ -8,7 +8,7 @@ echo "
 |_|  \___|\___\___/|_| |_(_)___/_| |_|"
 
 if [ -z "$1" ]
-    then
+then
     printf "\nUsage: ./recon.sh [option] <IP>\n"
     exit 1
 fi
@@ -16,14 +16,21 @@ fi
 printf "\n----- NMAP -----\n\n" > results
 
 echo "Running Nmap..."
-nmap $1 $2 | tail -n +5 | head -n -3 >> results
+if ! nmap $1 $2 | tail -n +5 | head -n -3 >> results &>/dev/null
+then
+    echo "Installing Nmap..."
+    sudo apt-get install -y nmap && nmap $1 $2 | tail -n +5 | head -n -3 >> results
+fi
 
 while read line
 do
     if [[ $line == *open* ]] && [[ $line == *http* ]]
     then
     echo "Running WhatWeb..."
-    whatweb $2 -v > temp
+    if ! whatweb $2 -v > temp &>/dev/null
+    then
+        echo "Installing WhatWeb..."
+        sudo apt-get install -y whatweb && whatweb $2 -v > temp
     fi
 done < results
 
